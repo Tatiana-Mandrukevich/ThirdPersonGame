69181097bc5a27f93d62da0064cd98c4eb23293b
Tatiana Mandrukevich <tatyana.zharikova.jazzteam@gmail.com>
2026-03-22 16:40:13 +0300

    added anime girl animation

--- ThirdPersonGame/Assets/GameAssets/Scripts/AnimatorParameters.cs
+++ ThirdPersonGame/Assets/GameAssets/Scripts/AnimatorParameters.cs
@@ -4,4 +4,5 @@ public class AnimatorParameters
 {
     public static readonly int IsFlying = Animator.StringToHash("IsFlying");
     public static readonly int IsMoving = Animator.StringToHash("IsMoving");
+    public static readonly int IsJumping = Animator.StringToHash("IsJumping");
 }
--- ThirdPersonGame/Assets/GameAssets/Scripts/PlayerAnimeGirlMover.cs
+++ ThirdPersonGame/Assets/GameAssets/Scripts/PlayerAnimeGirlMover.cs
@@ -1,24 +1,37 @@
-using DG.Tweening;
 using UnityEngine;
 
 public class PlayerAnimeGirlMover : MonoBehaviour
 {
     [SerializeField] private InputService _inputService;
     [SerializeField] private float _moveSpeed;
-    [SerializeField] private float _gravity;
+    [SerializeField] private float _gravity = -9.81f;
+    [SerializeField] private float _jumpVelocity = 5f;
+    [SerializeField] private float _groundedStickVelocity = -2f;
     [SerializeField] private float _moveInputDeadZone = 0.1f;
     [SerializeField] private Transform _body;
     [SerializeField] private Transform _animationTarget;
-    
+    [SerializeField] private float _visualGroundOffset = 0f;
+
     private CharacterController _characterController;
-    private Vector3 _moveDirection;
-    private float _moveDirectionY;
+    private float _verticalVelocity;
     private Animator _animator;
-    
+    private Vector3 _bodyStartLocalPosition;
+    private bool _isJumping;
+
     private void Awake()
     {
         _characterController = GetComponent<CharacterController>();
         _animator = GetComponent<Animator>();
+
+        if (_body == null || _body == transform)
+        {
+            var armature = transform.Find("Armature");
+            if (armature != null)
+                _body = armature;
+        }
+
+        if (_body != null && _body != transform)
+            _bodyStartLocalPosition = _body.localPosition;
     }
 
     private void OnEnable()
@@ -33,17 +46,45 @@ public class PlayerAnimeGirlMover : MonoBehaviour
 
     private void Update()
     {
-        _moveDirectionY = Mathf.Max(_gravity, _moveDirectionY + _gravity * Time.deltaTime);
-        var moveDirection = new Vector3(_inputService.MoveDirection.x, _moveDirectionY, _inputService.MoveDirection.y);
-        _characterController.Move(moveDirection * (Time.deltaTime * _moveSpeed));
-        _animator.SetBool(AnimatorParameters.IsMoving, _inputService.MoveDirection.magnitude > _moveInputDeadZone);
+        if (_characterController.isGrounded && _verticalVelocity < 0f)
+        {
+            _verticalVelocity = _groundedStickVelocity;
+
+            if (_isJumping)
+            {
+                _isJumping = false;
+                _animator.SetBool(AnimatorParameters.IsJumping, false);
+            }
+        }
+
+        _verticalVelocity += _gravity * Time.deltaTime;
+
+        Vector3 horizontalVelocity = new Vector3(_inputService.MoveDirection.x, 0f, _inputService.MoveDirection.y) * _moveSpeed;
+        Vector3 moveVelocity = horizontalVelocity + Vector3.up * _verticalVelocity;
+
+        _characterController.Move(moveVelocity * Time.deltaTime);
+
+        bool isMoving = _inputService.MoveDirection.magnitude > _moveInputDeadZone;
+        _animator.SetBool(AnimatorParameters.IsMoving, isMoving);
     }
-    
+
+    private void LateUpdate()
+    {
+        if (_body == null || _body == transform)
+            return;
+
+        var localPosition = _body.localPosition;
+        localPosition.y = _bodyStartLocalPosition.y + _visualGroundOffset;
+        _body.localPosition = localPosition;
+    }
+
     private void OnJump()
     {
         if (_characterController.isGrounded)
         {
-            _moveDirectionY = 1;
+            _verticalVelocity = _jumpVelocity;
+            _isJumping = true;
+            _animator.SetBool(AnimatorParameters.IsJumping, true);
         }
     }
 }
